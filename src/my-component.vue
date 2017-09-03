b57fab19353f7b4e4a4b0dc545514f0779c1dd64
bookstore <booookstore@icloud.com>
2017-09-03 10:48:09 +0900

    ADD：簡単なvueファイルを作成

---
+++ src/my-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="message">
+    メッセージ : {{ message }}
+  </p>
+</template>
+
+<style>
+.message {
+  color: #42b983;
+}
+</style>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      message: 'A'
+    }
+  }
+}
+</script>
